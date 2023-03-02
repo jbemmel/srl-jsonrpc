d13b226bb196376f31a8eb3ecb1c8992056abf76
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2023-03-02 09:46:38 -0600

    Add example of using JSON RPC with exclusive candidates and checkpoints

---
+++ robust_parallel_provisioning.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+# This example illustrates a way to use JSON RPC with multiple SRL nodes in parallel,
+# creating checkpoints and exclusive configuration sessions to get consistent
+# rollback
+
+import requests,json
+
+USERNAME = "admin"
+PASSWORD = "NokiaSrl1!"
+
+def _jsonrpcPost(node, json_data, timeout=None):
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+    geturl = f"https://{USERNAME}:{PASSWORD}@{node}:443/jsonrpc"
+    resp = requests.post(geturl, headers=headers, json=json_data, timeout=timeout, verify=False)
+    resp.raise_for_status()
+    print( json.dumps(resp.json()) )
+    return resp.json() if resp.text else ""
+
+def _jsonrpcRunCli(node,cmds):
+    data = {
+        "jsonrpc": "2.0",
+        "id": 0,
+        "method": "cli",
+        "params": {
+            "commands": cmds
+        }
+    }
+    return _jsonrpcPost(node,data)
+
+RANDOM_ID = "xyz"
+
+cmds = [
+ f"enter candidate exclusive name {RANDOM_ID}",
+ f"/tools system configuration generate-checkpoint name {RANDOM_ID}",
+
+ # Sample: Create a VLAN on both ports in a MH lag on 2 leaves
+ "/interface lag3 vlan-tagging true",
+ "/interface lag3 subinterface 0 vlan encap single-tagged vlan-id 10",
+
+ "commit now"
+]
+
+success = True
+NODES = ["clab-evpn-lab-leaf2","clab-evpn-lab-leaf3"]
+for node in NODES:
+  success = success and not "error" in _jsonrpcRunCli(node,cmds)
+
+for node in NODES:
+  _action = "clear" if success else "revert"
+  cmds2 = [
+    f"enter candidate exclusive name {RANDOM_ID}",
+    f"/tools system configuration checkpoint {RANDOM_ID} {_action}"
+  ]
+  _jsonrpcRunCli(node,cmds2)
